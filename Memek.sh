8c90ef97c8b0e189c68d6dec09d736445e7192a4
ACILOADING <52864713+tukangpulug199@users.noreply.github.com>
2019-08-16 17:51:28 +0700

    Update Memek.sh

--- Memek.sh
+++ Memek.sh
@@ -1,9 +1,8 @@
 !usr/bin/bash
-
 #Jangan Di Recode Bangsat!!
 #Coding By AC1L WAS H3R3
 #YouTube MR 4444
-
+clear
 bi='\033[34;1m' #biru
 i='\033[32;1m' #ijo
 pur='\033[35;1m' #purple
@@ -13,7 +12,6 @@ pu='\033[37;1m' #putih
 ku='\033[33;1m' #kuning
 mer='\033[41;97m' #Tepi
 st='\033[0m' #Stop
-clear
 toilet -f future "TEBAS WEBSITE" | lolcat
 echo $mer"=========================================="$st
 echo $i"Author : AC1L S34N"
